753d683e9a2a575c5e1e6c7e33297552baeb9203
aleahjalawig <jalawig0613@example.com>
2026-04-11 01:47:57 +0800

    3

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer';
 import { NavbarComponent } from '../../components/navbar/navbar';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Added for ngModel
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FooterComponent,NavbarComponent,RouterModule],
+  standalone: true,
+  imports: [FooterComponent, NavbarComponent, RouterModule, FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  // Data binding properties
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private router: Router) {}
+
+  onLogin() {
+    // Static credential check
+    if (this.username === 'admin' && this.password === '1234') {
+      console.log('Login successful');
+      this.router.navigate(['/home']);
+    } else {
+      this.errorMessage = 'Invalid username or password. Try admin / 1234';
+    }
+  }
+}
